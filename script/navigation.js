41dd0849f6c8b8f930fa3dfd0deac2738ebef3ef
Nowak92 <85220895+Nowak92@users.noreply.github.com>
2022-04-06 14:18:37 +0200

    Navbar progress

--- script/navigation.js
+++ script/navigation.js
@@ -1,11 +1,20 @@
+let BurgerCrossWrapper = document.getElementById("burger-cross-wrapper")
 let bars = document.getElementById("burger")
+let cross = document.getElementById("cross")
 let hiddenNav = document.getElementById("hidden-navigation")
 
-bars.addEventListener("click", doSomething);
+BurgerCrossWrapper.addEventListener("click", popOutNavbar);
 
-function doSomething() {
+function popOutNavbar() {
     
-    if(hiddenNav.style.right != "white") {
-        console.log("bars.style.color")
+    if(hiddenNav.style.left == "-100vw") {
+        hiddenNav.style.left = "0vw"
+        bars.style.display = "none"
+        cross.style.display = "block"
+    }
+    else {
+        hiddenNav.style.left = "-100vw"
+        bars.style.display = "block"
+        cross.style.display = "none"
     }
 }
