7c24e5820acf98b58bff6c97e6b72aeb47ee3dba
Nowak92 <85220895+Nowak92@users.noreply.github.com>
2022-04-22 13:50:37 +0200

    Card section finished

---
+++ script/card-section.js
@@ -0,0 +1,20 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var foldOut = this.previousElementSibling;
+    if (foldOut.style.height === "0px") {
+      foldOut.style.height = "350px";
+      foldOut.style.margin = "25px 20px 0 20px";
+      this.style.transform = "rotate(180deg)"
+      this.firstElementChild.style.display = "none" 
+    } else {
+      foldOut.style.height = "0px";
+      foldOut.style.margin = "0 20px 0 20px";
+      this.style.transform = "rotate(0deg)"
+      this.firstElementChild.style.display = "block" 
+    }
+  });
+}
