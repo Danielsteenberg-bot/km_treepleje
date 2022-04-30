677201071b868f659005a178e52c85c9e0b73793
Nowak92 <85220895+Nowak92@users.noreply.github.com>
2022-04-30 19:31:15 +0200

    Lot of stuff added

--- script/navigation.js
+++ script/navigation.js
@@ -16,5 +16,23 @@ function popOutNavbar() {
         hiddenNav.style.left = "-100vw"
         bars.style.display = "block"
         cross.style.display = "none"
+        servicesDivWrapper.style.height = "0px"
+        serviceArrow.style.transform = "rotate(0deg)"
     }
 }
+
+let servicesMenuPoint = document.querySelector(".services")
+let servicesDivWrapper = document.querySelector(".navigation-accordion")
+let serviceArrow = document.querySelector(".fold-out-arrow")
+
+servicesMenuPoint.addEventListener("click", function() {
+    if (servicesDivWrapper.style.height <= "0px") {
+        servicesDivWrapper.style.height = "250px"
+        serviceArrow.style.transform = "rotate(180deg)"
+    }
+    else {
+        servicesDivWrapper.style.height = "0px"
+        serviceArrow.style.transform = "rotate(0deg)"
+    }
+})
+console.log(servicesMenuPoint)
