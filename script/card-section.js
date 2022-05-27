3ce85e3faf7dca19af0eb5b55d101379abe79a0a
Nowak92 <85220895+Nowak92@users.noreply.github.com>
2022-05-27 02:00:41 +0200

    animations changed

--- script/card-section.js
+++ script/card-section.js
@@ -12,13 +12,34 @@ window.addEventListener("scroll", function(){
 let cards = document.querySelectorAll(".card")
 window.addEventListener("scroll", function(){
 for (let i = 0; i < cards.length; i++) {
-  let cardDistanceFromTop = cards[i].getBoundingClientRect().y - window.innerHeight
-  console.log("card" + " " + i + " " + cardDistanceFromTop)
+  let cardDistanceFromTop = cards[i].getBoundingClientRect().y - window.innerHeight * 0.9
 
- 
-  
-  if (cardDistanceFromTop <= 0 - 100) {
+  if (cardDistanceFromTop <= 0) {
     cards[i].classList.add("cards-moving")
   }
 }
+})
+
+/* Card section Cards hover zoom */
+let cardIcon = document.querySelectorAll(".card-icon")
+
+for (let i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("mouseover", function() {
+    cardIcon[i].style.transform = "scale(1.05)"
+  })
+}
+for (let i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("mouseout", function() {
+    cardIcon[i].style.transform = "scale(1.0)"
+  })
+}
+
+
+window.addEventListener("scroll", function(){
+  for (let i = 0; i < cards.length; i++) {
+    let measuredCard = cards[0].getBoundingClientRect().y - window.innerHeight
+    console.log(measuredCard)
+    if(measuredCard <= window.innerHeight) {
+    }
+  }
 })
